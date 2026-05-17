36c4d6598619c1c9345e0815b3d13c567c280e12
Shubham Kumar <shubham620409@gmail.com>
2026-05-17 16:58:31 +0530

    Brute Force

--- 10 Sliding Window & Two Pointer Combined Problems/J01LongestSubstring.java
+++ 10 Sliding Window & Two Pointer Combined Problems/J01LongestSubstring.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class J01LongestSubstring {
-    // My approach
+    // My approach XX Not working
     public static int longestSubstring(String s) {
         Set<Character> set = new HashSet<>();
         int longest = 0;
@@ -21,15 +21,39 @@ public class J01LongestSubstring {
         return Math.max(longest, current);
     }
 
+    // My approach 2 -> Generate all substring
+    public static int longestSubstring2(String s) {
+        int longest = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            Set<Character> seen = new HashSet<>();
+            for (int j = i; j < n; j++) {
+                char currentCharacter = s.charAt(j);
+
+                if (seen.contains(currentCharacter))
+                    break;
+
+                seen.add(currentCharacter);
+                longest = Math.max(longest, j - i + 1);
+            }
+        }
+        return longest;     
+    }
+
     public static void main(String args[]) {
         String s1 = "abcabcbb";
         String s2 = "bbbbb";
-        String s3 = "pwwkew";
+        String s3 = "dvdf";
         String s4 = "abc";
         System.out.println("Approach 1:- " + longestSubstring(s1));
         System.out.println("Approach 1:- " + longestSubstring(s2));
-        System.out.println("Approach 1:- " + longestSubstring(s3));
+        System.out.println("Approach 1:- " + longestSubstring(s3)+" X 3");
         System.out.println("Approach 1:- " + longestSubstring(s4));
+        System.out.println("-".repeat(40));
+        System.out.println("Approach 1:- " + longestSubstring2(s1));
+        System.out.println("Approach 1:- " + longestSubstring2(s2));
+        System.out.println("Approach 1:- " + longestSubstring2(s3));
+        System.out.println("Approach 1:- " + longestSubstring2(s4));
         System.out.println("-".repeat(40));
     }
 }
