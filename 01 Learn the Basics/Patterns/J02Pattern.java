1d29551d33e423d729c40d6dcf647af502b02592
Shubham Kumar <shubham620409@gmail.com>
2026-06-16 13:17:48 +0530

    Create J02Pattern.java

---
+++ 01 Learn the Basics/Patterns/J02Pattern.java
@@ -0,0 +1,21 @@
+// *
+// **
+// ***
+// ****
+// *****
+package Patterns;
+
+public class J02Pattern {
+    public static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        pattern(5);
+    }
+}
