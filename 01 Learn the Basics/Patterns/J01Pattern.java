8f780636108704d8de6f3e1b4ef190793808384c
Shubham Kumar <shubham620409@gmail.com>
2026-06-16 13:01:12 +0530

    Create J01Pattern.java

---
+++ 01 Learn the Basics/Patterns/J01Pattern.java
@@ -0,0 +1,14 @@
+public class J01Pattern {
+    public static void pattern(int n) {
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        pattern(5);
+    }
+}
