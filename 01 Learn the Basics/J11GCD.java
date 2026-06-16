7ac2a67f3f8b075b8e21b621793d1df1f4472d92
Shubham Kumar <shubham620409@gmail.com>
2026-06-16 17:40:04 +0530

    Create J11GCD.java

---
+++ 01 Learn the Basics/J11GCD.java
@@ -0,0 +1,16 @@
+public class J11GCD {
+    public static int GCD(int n1, int n2) {
+        int min = n1 < n2 ? n1 : n2;
+        for (int i = min; i > 1; i--) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(GCD(15, 10));
+        System.out.println(GCD(9, 8));
+    }
+}
