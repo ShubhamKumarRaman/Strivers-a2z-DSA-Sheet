0612e6be653ca981526e835088ca5029698460cd
Shubham Kumar <shubham620409@gmail.com>
2026-06-16 13:03:12 +0530

    Update J01Pattern.java

--- 01 Learn the Basics/Patterns/J01Pattern.java
+++ 01 Learn the Basics/Patterns/J01Pattern.java
@@ -1,7 +1,9 @@
+package Patterns;
+
 public class J01Pattern {
     public static void pattern(int n) {
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("*");
             }
             System.out.println();
