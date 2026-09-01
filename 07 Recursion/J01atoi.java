78ff45c6963b3d369d2097c84c5e0e65591c38ae
Shubham Kumar <shubham620409@gmail.com>
2026-09-01 20:36:03 +0530

    Update J01atoi.java

--- 07 Recursion/J01atoi.java
+++ 07 Recursion/J01atoi.java
@@ -20,8 +20,51 @@ public class J01atoi {
         return negative ? result * -1 : result;
     }
 
+    // Approach 2
+    public static int myAtoi2(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+        int i = 0;
+
+        // Skip whitespace
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        if (i == n) {
+            return 0;
+        }
+
+        // Handle sign
+        int sign = 1;
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        // Convert digit
+        int result = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = Character.getNumericValue(s.charAt(i));
+
+            // Check overflow before updating result
+            if ((result > Integer.MAX_VALUE / 10)
+                    || (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+        return sign * result;
+    }
+
     public static void main(String args[]) {
         System.out.println(myAtoi("1337c0d3"));
         System.out.println(myAtoi("0-1"));
+        System.out.println(myAtoi2("-91283472332"));
     }
 }
