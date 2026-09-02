420fcc9913a4c657693c3bd0679d29f9fdfeeecd
Shubham Kumar <shubham620409@gmail.com>
2026-09-02 07:31:18 +0530

    using recursino

--- 07 Recursion/J02pow.java
+++ 07 Recursion/J02pow.java
@@ -16,15 +16,38 @@ public class J02pow {
         }
         return x * (getPow(x, n - 1));
     }
-    
+
     // Approach 2:- using Math.pow() method
-    public static double myPow2(double x, int n){
+    public static double myPow2(double x, int n) {
         return (double) Math.pow(x, n);
+    }
+
+    // Approach 3:- using recursion
+    public static double myPow3(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return getPow3(x, N);
+    }
+
+    private static double getPow3(double x, long n) {
+        if (n == 0)
+            return 1.0;
+
+        double half = getPow3(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
     }
 
     public static void main(String args[]) {
         System.out.println("Approach 1:- " + myPow(2.00, 10));
         System.out.println("Approach 1:- " + myPow(2.10000, 3));
         System.out.println("Approach 2:- " + myPow2(2.00, -200000000));
+        System.out.println("Approach 3:- " + myPow3(2.00000, -2));
     }
 }
