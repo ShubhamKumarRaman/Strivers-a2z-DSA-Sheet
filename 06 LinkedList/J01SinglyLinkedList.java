8ba5c30211afddff4fc052d540e5ab9394f511b7
Shubham Kumar <shubham620409@gmail.com>
2026-09-03 15:18:18 +0530

    Update J01SinglyLinkedList.java

--- 06 LinkedList/J01SinglyLinkedList.java
+++ 06 LinkedList/J01SinglyLinkedList.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-class Node {
-    int data;
-    Node next;
-
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class J01SinglyLinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     Node head;
 
     // Insertion operation
